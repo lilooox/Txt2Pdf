5f1738a8a13ab04fd0086a45099a30fb0010064d
fengdu <fengdu_xiangyang@163.com>
2017-07-18 09:02:24 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using Aspose.Pdf;
+using Aspose.Pdf.Facades;
+using Aspose.Pdf.Text;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Txt2Pdf.Utilities;
+using System.Web;
+using System.Text.RegularExpressions;
+
+namespace Txt2Pdf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////The text file 
+            string filePath = @"C:\Export\110689.txt";
+            string outputPath = @"C:\Export\export.pdf";
+
+
+            // Create a new PdfWriter
+            PdfWriter pdfWriter = new PdfWriter(900.0f, 1190.0f, 20.0f, 12.0f);
+
+            //Write to a PDF format file
+            pdfWriter.Write(filePath, outputPath);
+
+
+
+
+        }
+    }
+
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+﻿## c# convert txt to pdf
+> 参考：https://www.codeproject.com/Articles/12266/Convert-a-Text-File-to-a-PDF-File
+
+
+### 原文不支持中文等....
+参考了pdf 1.4 标准 做了修改，可以支持中文
+
